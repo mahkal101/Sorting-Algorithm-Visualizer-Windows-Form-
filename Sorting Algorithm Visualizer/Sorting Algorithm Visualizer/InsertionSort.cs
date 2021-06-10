489287a50e4417d7211e7a384eaadbfc8536539a
Michael Saladino <52329398+mahkal101@users.noreply.github.com>
2021-06-10 13:45:45 -0400

    Add files via upload
    Initial Commit

---
+++ Sorting Algorithm Visualizer/Sorting Algorithm Visualizer/InsertionSort.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting_Algorithm_Visualizer
+{
+    class InsertionSort: ISortEngine
+    {
+        private int[] TheArray;
+        private Graphics g;
+        private int MaxVal;
+        Brush BlackBrush = new System.Drawing.SolidBrush(Color.Teal);
+        Brush WhiteBrush = new System.Drawing.SolidBrush(Color.PaleGreen);
+
+
+
+
+
+        public InsertionSort(int[] TheArray_In, Graphics g_In, int MaxVal_In)
+        {
+            TheArray = TheArray_In;
+            g = g_In;
+            MaxVal = MaxVal_In;
+        }
+
+        public void NextStep()
+        {
+
+            int n = TheArray.Length;
+            for (int i = 1; i < n; ++i)
+            {
+                int key = TheArray[i];
+                int j = i - 1;
+
+                // Move elements of TheArray,
+                // that are greater than key,
+                // to one position ahead of
+                // their current position
+                while (j >= 0 && TheArray[j] > key)
+                {
+                    TheArray[j + 1] = TheArray[j];
+                    DrawBar(j, TheArray[j]);
+                    j = j - 1;
+                }
+                TheArray[j + 1] = key;
+                DrawBar(i, TheArray[i]);
+            }
+        }
+        private void DrawBar(int position, int height)
+        {
+            g.FillRectangle(BlackBrush, position, 0, 1, MaxVal);
+            g.FillRectangle(WhiteBrush, position, MaxVal - TheArray[position], 1, MaxVal);
+        }
+
+
+        public bool IsSorted()
+        {
+            for (int i = 0; i < TheArray.Count() - 1; i++)
+            {
+                if (TheArray[i] > TheArray[i + 1]) return false;
+            }
+            return true;
+        }
+        public void ReDraw()
+        {
+            for (int i = 0; i < TheArray.Count() - 1; i++)
+            {
+                g.FillRectangle(new System.Drawing.SolidBrush(Color.Navy), i, MaxVal - TheArray[i], 1, MaxVal);
+
+            }
+
+        }
+    }
+}
---
+++ Sorting Algorithm Visualizer/Sorting Algorithm Visualizer/MergeSort.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sorting_Algorithm_Visualizer
+{
+    class MergeSort : ISortEngine
+    {
+        private int[] TheArray;
+        private Graphics g;
+        private int MaxVal;
+        private int left;
+        private int right;
+        Brush BlackBrush = new System.Drawing.SolidBrush(Color.Teal);
+        Brush WhiteBrush = new System.Drawing.SolidBrush(Color.PaleGreen);
+
+
+        public MergeSort(int[] TheArray_In, Graphics g_In, int MaxVal_In)
+        {
+            TheArray = TheArray_In;
+            g = g_In;
+            MaxVal = MaxVal_In;
+            left = 0;
+            right = TheArray.Length - 1;
+        }
+
+        public void NextStep()
+        {
+            Sort(TheArray,left,right);
+        }
+
+        private void Sort(int[] TheArray, int left, int right)
+        {
+            if (left < right)
+            {
+                // Find the middle point
+                 
+                int m = left + (right - left) / 2;
+
+                // Sort first and second halves
+
+                Sort(TheArray, left, m);
+                Sort(TheArray, m + 1, right);
+
+                // Merge the sorted halves
+                merge(TheArray, left, m, right);
+            }
+        }
+
+        private void DrawBar(int position, int height)
+        {
+            g.FillRectangle(BlackBrush, position, 0, 1, MaxVal);
+            g.FillRectangle(WhiteBrush, position, MaxVal - TheArray[position], 1, MaxVal);
+        }
+
+
+        public bool IsSorted()
+        {
+            for (int i = 0; i < TheArray.Count() - 1; i++)
+            {
+                if (TheArray[i] > TheArray[i + 1]) return false;
+            }
+            return true;
+        }
+        public void ReDraw()
+        {
+            for (int i = 0; i < TheArray.Count() - 1; i++)
+            {
+                g.FillRectangle(new System.Drawing.SolidBrush(Color.Navy), i, MaxVal - TheArray[i], 1, MaxVal);
+            }
+
+        }
+        void merge(int[] TheArray, int l, int m, int r)
+        {
+            
+            // Find sizes of two
+            // subarrays to be merged
+            int n1 = m - l + 1;
+            int n2 = r - m;
+
+            // Create temp arrays
+            int[] L = new int[n1];
+            int[] R = new int[n2];
+            int i, j;
+
+            // Copy data to temp arrays
+            for (i = 0; i < n1; ++i)
+                L[i] = TheArray[l + i];
+            for (j = 0; j < n2; ++j)
+                R[j] = TheArray[m + 1 + j];
+
+            // Merge the temp arrays
+
+            // Initial indexes of first
+            // and second subarrays
+            i = 0;
+            j = 0;
+
+            // Initial index of merged
+            // subarry array
+            int k = l;
+            while (i < n1 && j < n2)
+            {
+                if (L[i] <= R[j])
+                {
+                    TheArray[k] = L[i];
+                    DrawBar(k, TheArray[k]);
+                    Thread.Sleep(1);
+                    i++;
+                }
+                else
+                {
+                    TheArray[k] = R[j];
+                    DrawBar(k, TheArray[k]);
+                    Thread.Sleep(1);
+                    j++;
+                }
+                k++;
+            }
+
+            // Copy remaining elements of L[] if any and draw
+            // 
+            while (i < n1)
+            {
+                TheArray[k] = L[i];
+                DrawBar(k, TheArray[k]);
+
+                i++;
+                k++;
+            }
+
+            // Copy remaining elements of R[] if any and draw
+            // 
+            while (j < n2)
+            {
+                TheArray[k] = R[j];
+                DrawBar(k, TheArray[k]);
+
+                j++;
+                k++;
+            }
+        }
+    }
+}
